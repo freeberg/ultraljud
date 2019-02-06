8e6bc698e9a12498b7f711e5e7b5d0cb62a1f27a
Christoffer Arnlund <christoffer.arnlund@ibm.com>
2019-02-06 23:28:34 +0100

    Refactor

--- project1_128.m
+++ project1_128.m
@@ -1,5 +1,5 @@
 clear all
-%load PostRF_Carotid.mat
+load PostRF_Carotid.mat
 
 postRF_Carotid = PostRF;
 
@@ -14,14 +14,6 @@ hold on
 plot(y1)
 plot(y30)
 legend('y1','y30')
-%plot(postRF_Carotid.Signal(1,:),postRF_Carotid.Signal(:,1),'.')
-%Avbilda en linje
-%64 linjer använder den sig av 2048*64 
-%Räkna ut gångvägar, tid och samplingsfrekvenser
-%load preRF_ImageA.mat
-postRF_ImageA = PostRF;
-%plot(postRF_ImageA.Signal(:,1))
-%Varje punkt summering av 64 
 %Gör om 128 gånger
 %Sen har vi bilden
 %%
@@ -33,6 +25,7 @@ theta = zeros(2048,64,nrOfLines);
 focusDistace = zeros(2048,nrOfLines);
 reflectedTravelTime = zeros(2048,64,nrOfLines);
 numberOfSamplesDelay = zeros(2048,64,nrOfLines); 
+reflectedTravelDistance = zeros(2048,64,nrOfLines);
 newSignal = zeros(2048,nrOfLines);
 for x= 1:nrOfLines
    
@@ -42,36 +35,17 @@ for x= 1:nrOfLines
     end
 
     distanceBetweenElements = 0;
+    %Only need to go to 32? Since we're always it's a mirror reflection.
     for i=1:64
         for j= 1:2048
             theta(j,i,x) = atand(distanceBetweenElements/focusDistace(j,x));
-        end
-        distanceBetweenElements = distanceBetweenElements + preBeamformed.Pitch;
-    end
-
-    %Only need to go to 32? Since we're always it's a mirror reflection.
-    reflectedTravelDistance = zeros(2048,64,x);
-    distanceBetweenElements = 0;
-    for i=1:64
-        for j= 1:2048
             reflectedTravelDistance(j,i,x) = sqrt(distanceBetweenElements^2 + focusDistace(j,x)^2);
-        end
-        distanceBetweenElements = distanceBetweenElements + preBeamformed.Pitch;
-    end
-
-    
-    for i=1:64
-        for j= 1:2048
             reflectedTravelTime(j,i,x) = reflectedTravelDistance(j,i,x) / preBeamformed.SoundVel;
-        end
-    end
-
-    
-    for i=1:64
-        for j= 1:2048
             numberOfSamplesDelay(j,i,x) = round(reflectedTravelTime(j,i,x) /reflectedTravelTime(1,1,x))-j;
         end
+        distanceBetweenElements = distanceBetweenElements + preBeamformed.Pitch;
     end
+    
     signal = preBeamformed.Signal(:,:,x);
     
     for i=1:2048
@@ -81,7 +55,7 @@ for x= 1:nrOfLines
            elementDiff = abs(j-32)+1;
            amountOfSampleDelay = numberOfSamplesDelay(i,elementDiff,x);
            if(i+amountOfSampleDelay < 2049)
-            sumSignal = sumSignal + elementDiff/32* signal(i+amountOfSampleDelay,j);
+            sumSignal = sumSignal + elementDiff/16* signal(i+amountOfSampleDelay,j);
            end
         end
         %sumSignal        
